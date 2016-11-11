6f1bae4d4547f104caa38b863f541a70322fe225
JMedinilla <javimedinilla@gmail.com>
2016-11-11 10:54:32 +0100

    [2016-11-11 / 10.54] Right row implemented on Meeting List

--- app/src/main/java/com/jmedinilla/pi/condominapp/adapters/Adapter_List_Meetings.java
+++ app/src/main/java/com/jmedinilla/pi/condominapp/adapters/Adapter_List_Meetings.java
@@ -2,6 +2,7 @@ package com.jmedinilla.pi.condominapp.adapters;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,7 +20,7 @@ import java.util.List;
  * <p>
  * Adapter for the list in the meetings screen
  */
-public class Adapter_List_Meetings extends RecyclerView.Adapter<Adapter_List_Meetings.MeetingViewHolder> {
+public class Adapter_List_Meetings extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     private List<Model_Meeting> meetings;
     private Context ctxt;
@@ -30,18 +31,44 @@ public class Adapter_List_Meetings extends RecyclerView.Adapter<Adapter_List_Mee
     }
 
     @Override
-    public MeetingViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View item = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_meetings_left, null);
-        return new MeetingViewHolder(item);
+    public int getItemViewType(int position) {
+        return position % 2 * 2;
     }
 
     @Override
-    public void onBindViewHolder(MeetingViewHolder holder, int position) {
-        String month = (String) android.text.format.DateFormat.format("MMM", meetings.get(position).getMe_date());
-        String year = (String) android.text.format.DateFormat.format("yyyy", meetings.get(position).getMe_date());
-        String day = (String) android.text.format.DateFormat.format("dd", meetings.get(position).getMe_date());
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemLeft = null;
+        View itemRight = null;
 
-        holder.meetingDate.setText(day + " " + month + " " + year);
+        switch (viewType) {
+            case 0:
+                itemLeft = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_meetings_left, null);
+                break;
+            case 2:
+                itemRight = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_meetings_right, null);
+                break;
+        }
+
+        if (viewType == 0) {
+            return new MeetingViewHolderLeft(itemLeft);
+        } else {
+            return new MeetingViewHolderRight(itemRight);
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        String month = (String) DateFormat.format("MMM", meetings.get(position).getMe_date());
+        String year = (String) DateFormat.format("yyyy", meetings.get(position).getMe_date());
+        String day = (String) DateFormat.format("dd", meetings.get(position).getMe_date());
+
+        if (position % 2 == 0) {
+            MeetingViewHolderLeft holderLeft = (MeetingViewHolderLeft) holder;
+            ((MeetingViewHolderLeft) holder).meetingDateLeft.setText(day + " " + month + " " + year);
+        } else {
+            MeetingViewHolderRight holderRight = (MeetingViewHolderRight) holder;
+            ((MeetingViewHolderRight) holder).meetingDateRight.setText(day + " " + month + " " + year);
+        }
     }
 
     @Override
@@ -49,13 +76,23 @@ public class Adapter_List_Meetings extends RecyclerView.Adapter<Adapter_List_Mee
         return meetings.size();
     }
 
-    static class MeetingViewHolder extends RecyclerView.ViewHolder {
-        TextView meetingDate;
+    private static class MeetingViewHolderLeft extends RecyclerView.ViewHolder {
+        TextView meetingDateLeft;
 
-        MeetingViewHolder(View item) {
+        MeetingViewHolderLeft(View item) {
             super(item);
 
-            meetingDate = (TextView) item.findViewById(R.id.rowMeeLeft_txtDate);
+            meetingDateLeft = (TextView) item.findViewById(R.id.rowMeeLeft_txtDate);
+        }
+    }
+
+    private static class MeetingViewHolderRight extends RecyclerView.ViewHolder {
+        TextView meetingDateRight;
+
+        MeetingViewHolderRight(View item) {
+            super(item);
+
+            meetingDateRight = (TextView) item.findViewById(R.id.rowMeeRight_txtDate);
         }
     }
 
