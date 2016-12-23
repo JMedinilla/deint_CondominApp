1d5f3710119cb5544b3f6559633c10d59abcdcee
JMedinilla <javimedinilla@gmail.com>
2016-12-23 21:34:22 +0100

    (2016-12-23 / 21.34)

--- app/src/main/java/com/jmedinilla/pi/condominapp/presenters/Presenter_Diary.java
+++
@@ -1,9 +0,0 @@
-package com.jmedinilla.pi.condominapp.presenters;
-
-/**
- * Class created by JMedinilla on 2016-11-06
- * <p>
- * Presenter for the diary form
- */
-public class Presenter_Diary {
-}
